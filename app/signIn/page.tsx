5d0341e4744e53bcfa3fdae3876cf57459527ae4
alexsharich <alexsharich@gmail.com>
2023-07-22 15:22:16 +0200

    refactor: add components

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -1,40 +1,24 @@
+'use client';
+
 import Link from 'next/link';
 import style from './SignIn.module.css';
-import { MouseEvent, useState } from 'react';
+import { Text } from '@/components/text/Text';
+import { Links } from '@/components/links/Links';
 import { Congratulations } from '@/components/congratulations/Congratulations';
+import { useState } from 'react';
+import Image from 'next/image';
 
 const SignIn = () => {
-    const confirm = false;
+    const [firstConfirm, setFirstConfirm] = useState<boolean>(false);
 
-    if (!confirm) {
-        return (
-            <div className={style.congratlationsBlock}>
-                <h1 className={style.congratulationsText} style={{ marginTop: '30px' }}>
-                    Congratulation
-                </h1>
-                <p className={style.congratulationsDescription} style={{ marginTop: '30px' }}>
-                    Your email has been confirmed
-                </p>
-                <button className={style.congratulationsButton} style={{ marginTop: '80px' }}>
-                    Sign In
-                </button>
-                <img
-                    src={'/assets/congratulations.png'}
-                    width={432}
-                    height={300}
-                    style={{ marginTop: '80px' }}
-                />
-            </div>
-        );
+    if (!firstConfirm) {
+        return <Congratulations setFirstConfirm={setFirstConfirm} />;
     }
 
     return (
         <div className={style.signInModal}>
-            <h1 className={style.signInTitle}>Sign In</h1>
-            <div className={style.links}>
-                <img src="/assets/google.png" alt="Sign up with google" width={36} height={36} />
-                <img src="/assets/gitHub.png" alt="Sign up with gitHub" width={36} height={36} />
-            </div>
+            <Text size="20" weight="700" text="Sign In" />
+            <Links />
             <form className={style.signInForm} action="">
                 <div className={style.labelInputBlock}>
                     <label htmlFor="">Email</label>
@@ -52,14 +36,16 @@ const SignIn = () => {
                         placeholder="Password"
                     />
                 </div>
-                <h3 className={style.signInForgotPasswordText} style={{ color: '#8D9094' }}>
-                    <Link href={'/'}>{`Forgot password`}</Link> {/* TODO LINK */}
-                </h3>
+
+                <Link href={'/'}>
+                    <Text size="14px" weight="400" color="#8D9094" text={`Forgot password`} />
+                </Link>
                 <button className={style.signInButton}>Sign In</button>
             </form>
-            <h3 className={style.signInQuestion}>{`Don't have an account?`}</h3>
+
+            <Text text={`Don't have an account?`} size="16" weight="400" />
             <Link href={'/'}>
-                <h3 style={{ color: '#397DF6' }}>Sign Up</h3>
+                <Text text="Sign Up" color="#397DF6" />
             </Link>
         </div>
     );
