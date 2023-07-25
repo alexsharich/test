b0194879477e7a1de8400f3ca5f107d17f35a161
alexsharich <alexsharich@gmail.com>
2023-07-25 20:38:59 +0200

    change next.js version @13.3.0

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
-module.exports = {
-    basePath: '',
-  }
+module.exports = nextConfig
